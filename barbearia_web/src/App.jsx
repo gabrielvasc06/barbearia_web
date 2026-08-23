5366d613bf731bd12ba805e14bd1dffcc9a85f92
gabrielvasc06 <vanderson261v@gmail.com>
2026-08-23 12:29:21 -0300

    feat: primeiro commit do projeto !

---
+++ barbearia_web/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '20px', textAlign: 'center' }}>
+      <h1>💈 Barbearia Web</h1>
+      <p> <h3>
+          Projeto AV1 em React + Vite
+        </h3>
+        
+        </p>
+    </div>
+  );
+}
